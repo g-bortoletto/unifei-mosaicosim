b4982f644f7cc77faed01fbaeb26d7b8b5b2a3e8
Guilherme Bortoletto <guilherme.mbortoletto@gmail.com>
2023-04-02 16:02:57 -0300

    Add image loading and displaying

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,4 @@
+#include <string.h>
 #define IMGUI_IMPLEMENTATION
 #define IMGUI_DEMO
 #include "../include/imgui/imgui_single_file.h"
@@ -12,9 +13,46 @@
 #include "../include/fonts/segoeui.h"
 #include "../include/sokol/sokol_imgui.h"
 
+#include "../include/nfd/nfd_win.cpp"
+
+#define STB_IMAGE_IMPLEMENTATION
+#include "../include/stb/stb_image.h"
+
 #define global static
 
 global int main_menu_bar_height;
+global int side_bar_width = 200;
+global int window_width;
+global int window_height;
+global float window_ratio;
+global int viewport_x;
+global int viewport_y;
+global int viewport_width;
+global int viewport_height;
+global float viewport_ratio;
+global sg_image main_image;
+global sg_pipeline pipeline;
+
+static sg_image load_image(const char *filename)
+{
+    int width, height, channels;
+    sg_image img = { SG_INVALID_ID };
+    unsigned char *data = stbi_load(filename, &width, &height, &channels, 4);
+    sg_image_desc image_desc = 
+    {
+        .width = width,
+        .height = height,
+        .min_filter = SG_FILTER_LINEAR,
+        .mag_filter = SG_FILTER_LINEAR,
+        .wrap_u = SG_WRAP_CLAMP_TO_EDGE,
+        .wrap_v = SG_WRAP_CLAMP_TO_EDGE,
+    };
+    image_desc.data.subimage[0][0].ptr = data;
+    image_desc.data.subimage[0][0].size = (size_t)(width * height * 4);
+    img = sg_make_image(&image_desc);
+    stbi_image_free(data);    
+    return img;
+}
 
 static void draw_main_menu_bar(void)
 {
@@ -22,7 +60,15 @@ static void draw_main_menu_bar(void)
     {
     	if (ImGui::BeginMenu("Arquivo"))
     	{
-    		if (ImGui::MenuItem("Abrir imagem...", "CTRL+O")) { }
+    		if (ImGui::MenuItem("Abrir imagem...", "CTRL+O")) 
+    		{
+    			nfdchar_t *file_path = 0;
+    			nfdresult_t result = NFD_OpenDialog("png;jpg;jpeg", 0, &file_path);
+    			if (result == NFD_OKAY)
+    			{
+                    main_image = load_image(file_path);
+    			}
+    		}
     		if (ImGui::MenuItem("Sair", "ALT+F4")) { sapp_request_quit(); }
     		ImGui::EndMenu();
     	}
@@ -52,6 +98,33 @@ static void draw_side_bar(void)
     	ImGuiWindowFlags_NoResize 
 |    	ImGuiWindowFlags_NoTitleBar);
     ImGui::End();
+}
+
+static void draw_main_image(void)
+{
+    if (main_image.id != SG_INVALID_ID)
+    {
+        sg_image_desc main_image_desc = sg_query_image_desc(main_image);
+        const float image_ratio = main_image_desc.width / (float)main_image_desc.height;
+        sgp_set_image(0, main_image);
+        float main_image_width = viewport_ratio <= image_ratio ? viewport_width : (viewport_height * (float)image_ratio);
+        float main_image_height = viewport_ratio > image_ratio ? viewport_height : (viewport_width / (float)image_ratio);
+        sgp_draw_textured_rect(
+            viewport_x, 
+            viewport_y, 
+            main_image_width, 
+            main_image_height);
+        if (ImGui::Begin("ratio debug"))
+        {
+            ImGui::Text("image width:     %f", main_image_width);
+            ImGui::Text("image height:    %f", main_image_height);
+            ImGui::Text("image ratio:     %f", image_ratio);
+            ImGui::Text("viewport width:  %d", viewport_width);
+            ImGui::Text("viewport height: %d", viewport_height);
+            ImGui::Text("viewport ratio:  %f", viewport_ratio);
+            ImGui::End();
+        }
+    }
 }
 
 static void init(void) 
@@ -85,8 +158,8 @@ static void init(void)
     img_desc.width = font_width;
     img_desc.height = font_height;
     img_desc.pixel_format = SG_PIXELFORMAT_RGBA8;
-    img_desc.wrap_u = SG_WRAP_CLAMP_TO_EDGE;
-    img_desc.wrap_v = SG_WRAP_CLAMP_TO_EDGE;
+    img_desc.wrap_u = SG_WRAP_CLAMP_TO_BORDER;
+    img_desc.wrap_v = SG_WRAP_CLAMP_TO_BORDER;
     img_desc.min_filter = SG_FILTER_LINEAR;
     img_desc.mag_filter = SG_FILTER_LINEAR;
     img_desc.data.subimage[0][0].ptr = font_pixels;
@@ -125,6 +198,7 @@ static void frame(void)
 
     draw_main_menu_bar();
     draw_side_bar();
+    draw_main_image();
 
 	sg_pass_action pass_action = { };
     sg_begin_default_pass(&pass_action, window_width, window_height);
@@ -137,6 +211,7 @@ static void frame(void)
 
 static void cleanup(void) 
 {
+    sg_destroy_image(main_image);
     simgui_shutdown();
     sgp_shutdown();
     sg_shutdown();
