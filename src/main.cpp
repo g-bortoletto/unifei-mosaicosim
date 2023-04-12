de2bc2c5d6a0e528340a87dfa7e82bb3f86a9887
Guilherme Bortoletto <guilherme.mbortoletto@gmail.com>
2023-04-12 18:00:21 -0300

    Adiciona carregamento de imagens

--- src/main.cpp
+++ src/main.cpp
@@ -35,23 +35,23 @@ global sg_pipeline pipeline;
 
 static sg_image load_image(const char *filename)
 {
-    int width, height, channels;
-    sg_image img = { SG_INVALID_ID };
-    unsigned char *data = stbi_load(filename, &width, &height, &channels, 4);
-    sg_image_desc image_desc = 
-    {
-        .width = width,
-        .height = height,
-        .min_filter = SG_FILTER_LINEAR,
-        .mag_filter = SG_FILTER_LINEAR,
-        .wrap_u = SG_WRAP_CLAMP_TO_EDGE,
-        .wrap_v = SG_WRAP_CLAMP_TO_EDGE,
-    };
-    image_desc.data.subimage[0][0].ptr = data;
-    image_desc.data.subimage[0][0].size = (size_t)(width * height * 4);
-    img = sg_make_image(&image_desc);
-    stbi_image_free(data);    
-    return img;
+	int width, height, channels;
+	sg_image img = { SG_INVALID_ID };
+	unsigned char *data = stbi_load(filename, &width, &height, &channels, 4);
+	sg_image_desc image_desc = 
+	{
+		.width = width,
+		.height = height,
+		.min_filter = SG_FILTER_LINEAR,
+		.mag_filter = SG_FILTER_LINEAR,
+		.wrap_u = SG_WRAP_CLAMP_TO_EDGE,
+		.wrap_v = SG_WRAP_CLAMP_TO_EDGE,
+	};
+	image_desc.data.subimage[0][0].ptr = data;
+	image_desc.data.subimage[0][0].size = (size_t)(width * height * 4);
+	img = sg_make_image(&image_desc);
+	stbi_image_free(data);    
+	return img;
 }
 
 static void draw_main_menu_bar(void)
