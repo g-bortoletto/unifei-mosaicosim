ade0716e07d6ab1b94fb4ea7d4d0b966e7489b6e
Guilherme Bortoletto <guilherme.mbortoletto@gmail.com>
2023-04-16 18:34:58 -0300

    Adiciona operações com pontos

--- src/point.cpp
+++ src/point.cpp
@@ -8,6 +8,16 @@ const float point_distance(const point_t p1, const point_t p2)
 	return sqrt(pr[0] * pr[0] + pr[1] * pr[1]);
 }
 
+const point_t point_difference(const point_t p1, const point_t p2)
+{
+	return { p2.x - p1.x, p2.y - p1.y };
+}
+
+const point_t point_sum(const point_t p1, const point_t p2)
+{
+	return { p2.x + p1.x, p2.y + p1.y };
+}
+
 const void point_move(shape_t *shape, const int index, const point_t amount)
 {
 	if (!shape) { return; }
@@ -15,5 +25,15 @@ const void point_move(shape_t *shape, const int index, const point_t amount)
 	{
 		shape->p[index].x += amount.x;
 		shape->p[index].y += amount.y;
+	}
+}
+
+const void point_set(shape_t *shape, const int index, const point_t new_position)
+{
+	if (!shape) { return; }
+	if (index < shape->p.size())
+	{
+		shape->p[index].x = new_position.x;
+		shape->p[index].y = new_position.y;
 	}
 }
--- src/point.h
+++ src/point.h
@@ -12,6 +12,14 @@ const point_t point(const float x, const float y);
 
 const float point_distance(const point_t p1, const point_t p2);
 
-const void point_move(shape_t *triangle, const int index, const point_t new_position);
+const point_t point_difference(const point_t p1, const point_t p2);
+
+const point_t point_sum(const point_t p1, const point_t p2);
+
+const void point_move(shape_t *triangle, const int index, const point_t amount);
+
+const void point_set(shape_t *teiangle, const int index, const point_t new_position);
+
+const point_t operator-(const point_t other);
 
 // --------------------------------------------------------------------------------------------------------------------
