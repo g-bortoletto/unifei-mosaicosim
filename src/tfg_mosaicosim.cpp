dc750f922747fe263137265d095de54d878326b8
Guilherme <guilherme.mbortoletto@gmail.com>
2023-11-20 10:16:55 -0300

    Change imgui font

--- src/tfg_mosaicosim.cpp
+++ src/tfg_mosaicosim.cpp
@@ -19,30 +19,30 @@
 
 #include "../res/fonts/segoeui.h"
 
-namespace MosaicoSim
+#include "tfg_types.h"
+
+namespace mosaico_sim
 {
 	struct
 	{
+		rectwhf main_window;
+		float frame_time;
+		float dpi_scale;
+
 		struct
 		{
-			simgui_image_desc_t imdesc;
-			simgui_image_t imimg;
-			ImTextureID imid;
-			sg_pass pass;
+			sg_pass_action pass_action;
 		}
-		offscreen;
-
-		ImVec2 workspaceSize;
-		ImVec2 workspacePosition;
+		display;
 	}
 	state;
 
-	void Init();
-	void Frame();
-	void Input(const sapp_event *e);
-	void Cleanup();
+	void init(void);
+	void frame(void);
+	void input(const sapp_event *e);
+	void cleanup(void);
 
-	void Init()
+	void init(void)
 	{
 		sg_setup(&(sg_desc)
 		{
@@ -50,157 +50,105 @@ namespace MosaicoSim
 			.logger.func = slog_func,
 		});
 
-		sgp_setup(&(sgp_desc){});
-
 		simgui_setup(&(simgui_desc_t)
 		{
-			// .no_default_font = false,
+			.no_default_font = true,
+			.logger.func = slog_func,
 		});
-		// auto &io = ImGui::GetIO();
-		// ImFontConfig fontCfg;
-		// fontCfg.FontDataOwnedByAtlas = true;
-		// fontCfg.OversampleH = 1;
-		// fontCfg.OversampleV = 1;
-		// fontCfg.RasterizerMultiply = 1.0f;
-		// io.Fonts->AddFontFromMemoryCompressedTTF(
-		// 	segoeui_compressed_data,
-		// 	segoeui_compressed_size,
-		// 	16.0f,
-		// 	&fontCfg);
-		// unsigned char *font_pixels;
-		// int font_width, font_height;
-		// io.Fonts->GetTexDataAsRGBA32(&font_pixels, &font_width, &font_height);
-		// sg_image_desc img_desc =
-		// {
-		// 	.width = font_width,
-		// 	.height = font_height,
-		// 	.pixel_format = SG_PIXELFORMAT_RGBA8,
-		// 	.data.subimage[0][0].ptr = font_pixels,
-		// 	.data.subimage[0][0].size = (size_t)(font_width * font_height * 4),
-		// };
-		// io.Fonts->TexID = (ImTextureID)(uintptr_t)sg_make_image(&img_desc).id;
 
-		state.offscreen.imdesc =
+		ImGui::StyleColorsLight(&ImGui::GetStyle());
+
+		auto& io = ImGui::GetIO();
+		ImFontConfig font_cfg;
+		font_cfg.FontDataOwnedByAtlas = false;
+		font_cfg.OversampleH = 2;
+		font_cfg.OversampleV = 2;
+		font_cfg.RasterizerMultiply = 1.0f;
+		io.Fonts->AddFontFromMemoryCompressedTTF(
+			segoeui_compressed_data, 
+			segoeui_compressed_size, 
+			16.0f, 
+			&font_cfg);
+
+		unsigned char* font_pixels;
+		int font_width, font_height;
+		io.Fonts->GetTexDataAsRGBA32(&font_pixels, &font_width, &font_height);
+		sg_image_desc img_desc = { };
+		img_desc.width = font_width;
+		img_desc.height = font_height;
+		img_desc.pixel_format = SG_PIXELFORMAT_RGBA8;
+		img_desc.data.subimage[0][0].ptr = font_pixels;
+		img_desc.data.subimage[0][0].size = font_width * font_height * 4;
+		sg_image font_img = sg_make_image(&img_desc);
+		sg_sampler_desc smp_desc = { };
+		smp_desc.min_filter = SG_FILTER_LINEAR;
+		smp_desc.mag_filter = SG_FILTER_LINEAR;
+		// for high dpi displays
+		// smp_desc.min_filter = SG_FILTER_NEAREST;
+		// smp_desc.mag_filter = SG_FILTER_NEAREST;
+		smp_desc.wrap_u = SG_WRAP_CLAMP_TO_EDGE;
+		smp_desc.wrap_v = SG_WRAP_CLAMP_TO_EDGE;
+		sg_sampler font_smp = sg_make_sampler(&smp_desc);
+		simgui_image_desc_t font_desc = { };
+		font_desc.image = font_img;
+		font_desc.sampler = font_smp;
+		io.Fonts->TexID = simgui_imtextureid(simgui_make_image(&font_desc));
+
+		state.display.pass_action.colors[0] = 
 		{
-			.image = sg_make_image(&(sg_image_desc)
-			{
-				.render_target = true,
-				.width = 128,
-				.height = 128,
-			}),
-			.sampler = sg_make_sampler(&(sg_sampler_desc)
-			{
-				.min_filter = SG_FILTER_LINEAR,
-				.mag_filter = SG_FILTER_LINEAR,
-				.wrap_u = SG_WRAP_CLAMP_TO_EDGE,
-				.wrap_v = SG_WRAP_CLAMP_TO_EDGE,
-			}),
+			.load_action = SG_LOADACTION_CLEAR,
+			.clear_value = { 0.33f, 0.33f, 0.33f, 1.0f },
 		};
-		state.offscreen.imimg = simgui_make_image(&state.offscreen.imdesc);
-
-		state.offscreen.imid = simgui_imtextureid(state.offscreen.imimg);
-
-		// state.offscreen.pass = sg_make_pass(&(sg_pass_desc)
-		// {
-		// 	.color_attachments[0].image = state.offscreen.imdesc.image,
-		// });
 	}
 
-	void Frame()
+	void frame(void)
 	{
-		float w = sapp_widthf();
-		float h = sapp_heightf();
-
-		sgp_begin(w, h);
+		state.main_window.w = sapp_width();
+		state.main_window.h = sapp_height();
+		state.frame_time = sapp_frame_duration();
+		state.dpi_scale = sapp_dpi_scale();
 
 		simgui_new_frame(&(simgui_frame_desc_t)
 		{
-			.width = (int)w,
-			.height = (int)h,
-			.delta_time = sapp_frame_duration(),
-			.dpi_scale = sapp_dpi_scale(),
+			.width = (int)state.main_window.w,
+			.height = (int)state.main_window.h,
+			.delta_time = state.frame_time,
+			.dpi_scale = state.dpi_scale,
 		});
 
-		ImGui::SetNextWindowPos({0, 0}, ImGuiCond_Once);
-		ImGui::SetNextWindowSize({200, 200}, ImGuiCond_Once);
-		if (ImGui::Begin("Workspace"))
+		ImGui::SetNextWindowPos({ 50.0f, 50.0f }, ImGuiCond_Once);
+		ImGui::SetNextWindowSize({ 300.0f, 300.0f }, ImGuiCond_Once);
+		if (ImGui::Begin("The Window"))
 		{
-			state.workspacePosition = ImGui::GetWindowPos();
-			state.workspaceSize = ImGui::GetWindowSize();
-			ImGui::Image(
-				state.offscreen.imid,
-				state.workspaceSize);
+			if (ImGui::Button("This is a test"))
+			{
+
+			}
 			ImGui::End();
 		}
 
-		// offscreen pass
-		sgp_begin(128, 128);
-		sgp_project(0, 128, 128, 0);
-		sgp_draw_filled_triangle(
-			0.0f, 0.0f, 
-			0.0f, 128.0f,
-			128.0f, 128.0f);
-		sg_begin_pass(state.offscreen.pass, &(sg_pass_action)
-		{
-			.colors[0].load_action = SG_LOADACTION_LOAD,
-			.colors[0].clear_value.r = 0.0f,
-			.colors[0].clear_value.g = 1.0f,
-			.colors[0].clear_value.b = 0.0f,
-			.colors[0].clear_value.a = 1.0f,
-		});
-		sgp_flush();
-		sgp_end();
-		sg_end_pass();
+		sg_begin_default_passf(
+			&state.display.pass_action,
+			state.main_window.w,
+			state.main_window.h);
 
-		// display pass
-		// sgp_push_transform();
-		// sgp_set_image(0, state.offscreen.imdesc.image);
-		// sgp_set_sampler(0, state.offscreen.imdesc.sampler);
-		// sgp_set_color(1.0f, 1.0f, 1.0f, 1.0f);
-		// sgp_draw_filled_rect(
-		// 	state.workspacePosition.x, 
-		// 	state.workspacePosition.y,
-		// 	state.workspaceSize.x,
-		// 	state.workspaceSize.y);
-		// sgp_rect dest_rect = 
-		// {
-		// 	state.workspacePosition.x,
-		// 	state.workspacePosition.y,
-		// 	state.workspaceSize.x,
-		// 	state.workspaceSize.y,
-		// };
-		// sgp_rect dest_rect = {0, 0, 128, 128};
-		// sgp_rect src_rect = {0, 0, 128, 128};
-		// sgp_draw_textured_rect(
-		// 	0,
-		// 	dest_rect,
-		// 	src_rect);
-		// sgp_reset_image(0);
-		// sgp_reset_sampler(0);
-		// sgp_pop_transform();
+		// applyresources here
 
-		sg_pass_action passAction = {};
-		sg_begin_default_pass(
-			&passAction,
-			w,
-			h);
-		sgp_reset_pipeline();
-		sgp_flush();
-		sgp_end();
+		// sg_draw here
+
 		simgui_render();
 		sg_end_pass();
 		sg_commit();
 	}
 
-	void Input(const sapp_event *e)
+	void input(const sapp_event *e)
 	{
 		simgui_handle_event(e);
 	}
 
-	void Cleanup()
+	void cleanup(void)
 	{
 		simgui_shutdown();
-		sgp_shutdown();
 		sg_shutdown();
 	}
 }
